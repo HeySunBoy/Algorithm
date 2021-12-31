a50c4374104d8f84b598cc02ac1cf5ccea69f5ac
GaryChen <tp841025@gmail.com>
2021-12-31 16:35:51 +0800

    Add 氣泡排序法

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 3, 44, 38, 5, 47, 15 };
+            bubbleSort(arr);
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        static int[] bubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
