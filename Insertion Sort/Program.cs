619e6bd0b22b6fdc1890c8a092b41c9024fb3aa3
GaryChen <tp841025@gmail.com>
2022-01-02 21:14:01 +0800

    1. 插入排序法

---
+++ Insertion Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 3, 44, 38, 5, 47, 15 };
+            selectionSort(arr);
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        static int[] selectionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var temp = arr[i];
+
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (temp < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                    }
+                    else
+                    {
+                        arr[j + 1] = temp;
+                        break;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
